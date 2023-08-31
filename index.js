f973da12dd16997670fc4275674d3047f80c9e8a
Harsh <harshshah692@gmail.com>
2023-08-31 17:26:17 +0530

    Add local storage

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const port = 3000;
 
 app.use(express.json());
 
+const storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    },
+    });
+    const upload = multer({ storage });
+
 app.get('/health', (req, res) => {
     res.json({ status: 'ok' });
   });
